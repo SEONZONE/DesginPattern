657e57d1070a389f03caba64942ffb6f3ea6883b
PARK SEONGJONG <tjdwhd2247@gmail.com>
2025-07-13 23:41:26 +0900

    feat: 디자인패턴 공부 > Decorator 패턴

---
+++ src/DecoratePattern/CondimentDecorator.java
@@ -0,0 +1,6 @@
+package DecoratePattern;
+
+public abstract class CondimentDecorator extends Beverage{
+    Beverage beverage;
+    public abstract String getDescription();
+}
---
+++ src/DecoratePattern/HouseBlend.java
@@ -0,0 +1,11 @@
+package DecoratePattern;
+
+public class HouseBlend extends Beverage{
+    public HouseBlend(){
+        description = "에스프레소";
+    }
+
+    public double cost(){
+        return 0.99;
+    }
+}
